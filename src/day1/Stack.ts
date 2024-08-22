e1c52d7ef60d650caf067c40d43e844f96449b24
MR-Elbarbary <121816435+MR-Elbarbary@users.noreply.github.com>
2024-08-22 20:31:55 +0300

    Complete Stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,42 @@
+type Node<T> ={
+    value:T;
+    next?:Node<T>;
+}
 export default class Stack<T> {
     public length: number;
-
+    private last?: Node<T>;
     
 
     constructor() {
+        this.last = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
+        const node = {value:item} as Node<T>;
+        if (this.length === 0) {
+            this.last = node
+        }
+        else{
+            node.next = this.last
+            this.last = node
+        }
+        this.length++;
 
 }
     pop(): T | undefined {
+        if (this.length === 0) {
+            return undefined
+        }
+        else{
+            const node = {value:this.last?.value, next: this.last} as Node<T>;
+            this.last = this.last?.next;
+            this.length--;
+            return node.value;
+        }
 
 }
     peek(): T | undefined {
-
+        return this.last?.value;
 }
 }
